0f37bc0818a44f75cccfbdf8935e066c2eee19fe
Chirag Khurana <khuranachirag95@gmail.com>
2015-07-24 21:05:23 +0530

    Introducing Content Provider Class

--- app/src/main/java/in/kicka55studios/popularmovies/data/MoviesContract.java
+++ app/src/main/java/in/kicka55studios/popularmovies/data/MoviesContract.java
@@ -1,5 +1,7 @@
 package in.kicka55studios.popularmovies.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -8,9 +10,19 @@ public class MoviesContract {
     public static final String API_KEY = "4af8cef0d0ad1b4b83f6b63839c5c0b3";
     public static final String CONTENT_AUTHORITY = "in.kicka55studios.popularmovies";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
     public static final String PATH_MOVIES = "movies";
+    public static final String PATH_POPULARITY = PATH_MOVIES + "/popularity";
+    public static final String PATH_RATING = PATH_MOVIES + "/rating";
+
 
     public static final class MoviesEntry implements BaseColumns {
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
+
         public static final String TABLE_NAME = "movies";
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
         public static final String COLUMN_MOVIE_ID = "movie_id";
@@ -22,5 +34,8 @@ public class MoviesContract {
         public static final String COLUMN_OVERVIEW = "overview";
         public static final String COLUMN_POSTER_PATH = "poster_path";
 
+        public static Uri buildMoviesUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 }
--- app/src/main/java/in/kicka55studios/popularmovies/data/MoviesProvider.java
+++ app/src/main/java/in/kicka55studios/popularmovies/data/MoviesProvider.java
@@ -2,37 +2,226 @@ package in.kicka55studios.popularmovies.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
-public class MoviesProvider extends ContentProvider{
+public class MoviesProvider extends ContentProvider {
+
+    static final int MOVIES = 100;
+    static final int MOVIES_POPULARITY = 101;
+    static final int MOVIES_RATING = 102;
+    static final int MOVIES_WITH_ID = 103;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MoviesDbHelper mOpenHelper;
+
+    static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MoviesContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, MoviesContract.PATH_MOVIES, MOVIES);
+        matcher.addURI(authority, MoviesContract.PATH_POPULARITY, MOVIES_POPULARITY);
+        matcher.addURI(authority, MoviesContract.PATH_RATING, MOVIES_RATING);
+        matcher.addURI(authority, MoviesContract.PATH_MOVIES + "/*", MOVIES_WITH_ID);
+
+        return matcher;
+
+    }
+
     @Override
     public boolean onCreate() {
+        mOpenHelper = new MoviesDbHelper(getContext());
         return false;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
+
+            // "movies"
+            case MOVIES: {
+                retCursor = mOpenHelper.getWritableDatabase().query(
+                        MoviesContract.MoviesEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+
+            // "movies/popularity"
+            case MOVIES_POPULARITY: {
+                retCursor = mOpenHelper.getWritableDatabase().query(
+                        MoviesContract.MoviesEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        MoviesContract.MoviesEntry.COLUMN_POPULARITY + " DESC",
+                        "20"
+                );
+                break;
+            }
+            // "movies/rating"
+            case MOVIES_RATING: {
+                retCursor = mOpenHelper.getWritableDatabase().query(
+                        MoviesContract.MoviesEntry.TABLE_NAME,
+                        projection,
+                        MoviesContract.MoviesEntry.COLUMN_VOTES + " > ?",
+                        new String[]{"2000"},
+                        null,
+                        null,
+                        MoviesContract.MoviesEntry.COLUMN_RATING + " DESC",
+                        "20"
+                );
+                break;
+            }
+            // "movies/*"
+            case MOVIES_WITH_ID: {
+                String id = uri.getPathSegments().get(1);
+                retCursor = mOpenHelper.getWritableDatabase().query(
+                        MoviesContract.MoviesEntry.TABLE_NAME,
+                        projection,
+                        MoviesContract.MoviesEntry.COLUMN_MOVIE_ID + " = ?",
+                        new String[]{id},
+                        null,
+                        null,
+                        null
+                );
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case MOVIES:
+                return MoviesContract.MoviesEntry.CONTENT_TYPE;
+            case MOVIES_POPULARITY:
+                return MoviesContract.MoviesEntry.CONTENT_TYPE;
+            case MOVIES_RATING:
+                return MoviesContract.MoviesEntry.CONTENT_TYPE;
+            case MOVIES_WITH_ID:
+                return MoviesContract.MoviesEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case MOVIES: {
+                long _id = db.insert(MoviesContract.MoviesEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = MoviesContract.MoviesEntry.buildMoviesUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case MOVIES:
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long _id = db.insert(MoviesContract.MoviesEntry.TABLE_NAME, null, value);
+                        if (_id != -1) {
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            default:
+                return super.bulkInsert(uri, values);
+        }
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+        int rowsDeleted;
+
+        if (selection == null)
+            selection = "1";
+
+        switch (sUriMatcher.match(uri)) {
+            case MOVIES: {
+                rowsDeleted = db.delete(MoviesContract.MoviesEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs);
+
+            }
+            break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsDeleted;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int rowsUpdated;
+
+        switch (sUriMatcher.match(uri)) {
+            case MOVIES: {
+                rowsUpdated = db.update(MoviesContract.MoviesEntry.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs);
+            }
+            break;
+
+            default:
+                throw new UnsupportedOperationException("Failed to update rows from " + uri);
+        }
+
+        if (rowsUpdated != 0 || selection == null) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsUpdated;
     }
 }
